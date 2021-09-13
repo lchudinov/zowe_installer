308a9447fb05b887d85610698cdd2f4811cdefbc
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-09-13 17:04:58 +0500

    Get launch components
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -5,14 +5,17 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/pkg/errors"
 )
 
 type Launcher struct {
-	rootDir     string
-	instanceDir string
+	rootDir          string
+	instanceDir      string
+	haInstanceId     string
+	launchComponents []string
 }
 
 func New() *Launcher {
@@ -22,11 +25,15 @@ func New() *Launcher {
 
 func (launcher *Launcher) Run(instanceDir string, haInstanceId string) error {
 	launcher.instanceDir = instanceDir
+	launcher.haInstanceId = haInstanceId
 	if _, err := os.Stat(instanceDir); err != nil {
 		return errors.Wrapf(err, "failed to find INSTANCE_DIR %s", instanceDir)
 	}
 	if err := launcher.findRootDir(); err != nil {
 		return errors.Wrapf(err, "failed to find ROOT_DIR")
+	}
+	if err := launcher.getLaunchComponents(); err != nil {
+		return errors.Wrapf(err, "failed to find launch components")
 	}
 	return nil
 }
@@ -46,5 +53,19 @@ func (launcher *Launcher) findRootDir() error {
 	}
 	launcher.rootDir = rootDir
 	log.Printf("ROOT_DIR = %s\n", rootDir)
+	return nil
+}
+
+func (launcher *Launcher) getLaunchComponents() error {
+	script := filepath.Join(launcher.rootDir, "bin", "internal", "get-launch-components.sh")
+	cmd := exec.Command(script, "-c", launcher.instanceDir, "-r", launcher.rootDir, "-i", launcher.haInstanceId)
+	output, err := cmd.Output()
+	if err != nil {
+		return errors.Wrapf(err, "failed to run %s", script)
+	}
+	launcher.launchComponents = strings.Split(strings.TrimSuffix(string(output), "\n"), ",")
+	if len(launcher.launchComponents) == 0 {
+		return errors.New("no launch components")
+	}
 	return nil
 }
