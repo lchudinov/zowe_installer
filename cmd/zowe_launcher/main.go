130c34a5b9bb596a3353d053154808c158eef865
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-10-09 18:15:05 +0500

    Use key and cert files
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- cmd/zowe_launcher/main.go
+++ cmd/zowe_launcher/main.go
@@ -22,19 +22,21 @@ func setupInterrutHandler(launcher *launcher.Launcher) {
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s INSTANCE_DIR HA_INSTANCE_ID\n", filepath.Base(os.Args[0]))
+	if len(os.Args) != 5 {
+		fmt.Printf("Usage: %s INSTANCE_DIR HA_INSTANCE_ID server.key server.cer\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
 	instanceDir := os.Args[1]
 	haInstanceId := os.Args[2]
+	key := os.Args[3]
+	cert := os.Args[4]
 	launcher := launcher.New()
 	if err := launcher.Run(instanceDir, haInstanceId); err != nil {
 		log.Fatalf("failed to run Zowe: %v", err)
 	}
 	setupInterrutHandler(launcher)
 	go func() {
-		if err := launcher.ListenAndServe(); err != nil {
+		if err := launcher.ListenAndServeTLS(cert, key); err != nil {
 			log.Fatal(err)
 		}
 	}()
