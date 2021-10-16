0bd01f5b8b1145a7fef6a79bf7edf4b51c479303
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-10-16 21:16:11 +0500

    Improve view
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- launcher-frontend/src/app/components/comps/comps.component.ts
+++ launcher-frontend/src/app/components/comps/comps.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
 import { Comp } from 'src/app/shared';
 
@@ -12,7 +13,7 @@ export class CompsComponent implements OnInit {
   comps$: Observable<Comp[]>;
 
   constructor(private api: ApiService) {
-    this.comps$ = this.api.getComponents();
+    this.comps$ = this.api.getComponents().pipe(map(comps => comps.sort((a, b) => a.name < b.name ? -1 : 1)));
   }
 
   ngOnInit(): void {
